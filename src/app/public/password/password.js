91d2a9824c0b4e53d83ed0a9d68e57615f8a61a2
Rapou <raphael.schmutz@heig-vd.ch>
2015-05-27 16:10:30 +0200

    Modal service : Edit publicspace

--- src/app/public/password/password.js
+++ src/app/public/password/password.js
@@ -1,54 +1,25 @@
 angular.module('public.password', [
     'public.password.directives'
 ])
-.factory('WegasModalService', function($q, $animate, ModalService){
-    var service = angular.copy(ModalService);
-
-    service.displayAModal = function(options) {
-        var deferred = $q.defer();
-        ModalService.showModal(options).then(function(modal) {
-            var box = $(".modal"),
-                shadow = $(".shadow");
-            $(document).keyup(function(e) {
-                if (e.keyCode == 27 && box.length >= 1) {
-                    modal.close.then(function() {
-                        alert('dddd');
-                    });
-                    // $animate.removeClass(shadow, "shadow--show");
-                    // $animate.removeClass(box, "modal--open");
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('wegas.public.password', {
+                url: '/password',
+                views: {
+                    "modal@wegas.public": {
+                        controller: 'PublicPasswordModalController'
+                    }
                 }
+
             });
-            $animate.addClass(box, "modal--open");
-            $animate.addClass(shadow, "shadow--show");
-            return deferred.resolve();
-        });
-        return deferred.promise;
-    };
-
-    return service;
-})
-.config(function ($stateProvider) {
-    $stateProvider
-        .state('wegas.public.password', {
-            url: '/password',
-            views: {
-        		"modal@wegas.public" :{
-            		controller: 'PublicPasswordModalController'
-            	}
-            }
-
-        })
-    ;
-})
-.controller("PublicPasswordModalController", function PublicPasswordModalController($animate, $state, WegasModalService) {
+    })
+    .controller("PublicPasswordModalController", function PublicPasswordModalController($state, WegasModalService) {
         WegasModalService.displayAModal({
             templateUrl: 'app/public/password/password.tmpl.html',
             controller: "ModalsController as modalsCtrl"
         }).then(function(modal) {
-            alert('a');
             modal.close.then(function(result) {
                 $state.go("^");
             });
         });
-    })
-;
+    });
